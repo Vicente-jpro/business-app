e672747b659969d8ba7b85d64c76b76d0c9672a1
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-02-14 16:06:09 +0100

    route changed

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'withdraw/account'
   resources :account_histories
   resources :accounts
-  devise_for :users
+  devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'} 
 
   get '/accounts/:id/user', to: 'accounts#user_accounts', as: 'user_accounts'
   get '/accounts/:number/withdraw', to: 'accounts#withdraw', as: 'withdraw'
