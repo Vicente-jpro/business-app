c57a318291442b48a601469c58b912595a32a0a4
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-02-20 17:44:40 +0100

    account controller

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -5,16 +5,15 @@ class AccountsController < ApplicationController
   #POST '/accounts/transference_now'
   def transference_now
     @account = Account.find_by_account_number(params[:number])                     
-
+   # @account_destination = Account.destination_account
+   debugger
     if account_transfere_now_params[:money].to_f > @account.money.to_f
       redirect_to "/accounts/"+@account.number.to_s+"/withdraw"
       flash[:notice]="Money should less than or equal to $ "+@account.money.to_s
     elsif account_transfere_now_params[:money].to_f < 0
       redirect_to "/accounts/"+@account.number.to_s+"/withdraw" 
-      flash[:notice]="Money should greater than or equal to $ "+0.to_s
+      flash[:notice]="Money should greater than or equal to $ 0"
     else
-    #logger.debug {"##############Last acount attributes hash: #{@account_params.attributes.inspect}########"}
-
 
       respond_to do |format|
         if @account.update(account_transfere_now_params)
@@ -36,13 +35,11 @@ class AccountsController < ApplicationController
     account.money = 0.0;
     @account_withdraw = account
     button_name("Take money")
- # logger.debug {"Last acount attributes hash: #{@account_withdraw.attributes.inspect}"}
   end
 
    # POST /accounts/:number/withdraw
   def transference
     @account = Account.find_by_account_number(params[:number])
-    #logger.debug {"##############Last acount attributes hash: #{btn[:transfer]}########"}
     button_name("Transfere money")
   end
 
@@ -72,15 +69,14 @@ class AccountsController < ApplicationController
 
   # PATCH/PUT /accounts/1 or /accounts/1.json
   def update
-      if account_transfere_now_params[:money].to_f > @account.money.to_f
+      if account_update_params[:money].to_f > @account.money.to_f
         redirect_to "/accounts/"+@account.number.to_s+"/withdraw"
         flash[:notice]="Money should less than or equal to $ "+@account.money.to_s
-      elsif account_params[:money].to_f < 0
+      elsif account_update_params[:money].to_f < 0
         redirect_to "/accounts/"+@account.number.to_s+"/withdraw" 
         flash[:notice]="Money should greater than or equal to $ "+0.to_s
       else
-      logger.debug {"##############Last acount attributes hash: #{@account.attributes.inspect}########"}
-  
+      
       respond_to do |format|
         if @account.update(account_update_params)
           format.html { redirect_to account_url(@account), notice: "Account was successfully updated." }
@@ -144,7 +140,7 @@ class AccountsController < ApplicationController
     end
     
     def account_transfere_now_params
-      params.require(:account).permit(:money, :number, :destination_account)
+      params.require(:account).permit(:money, :number, :destination_account_number)
     end
 
     def account_update_params
